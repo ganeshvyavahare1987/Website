9e4086ab6074d5e912e4b644fa487ba9fe083114
ganeshvyavahare1987 <147035483+ganeshvyavahare1987@users.noreply.github.com>
2024-06-24 15:10:03 +0400

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace CarnivalCinema
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+           // GlobalFilters.Filters.Add(new HashCodeCheckFilterAttribute());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
